3afb96ea854f88a52383f29e9b12f41a7419204f
2271738697 <2271738697>
2020-03-04 17:15:04 +0800

    springBoot安全框架练习

---
+++ src/main/java/com/example/security/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.security;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhoudb
+ * @date 2019/12/23 16:59
+ */
+@RestController
+public class HelloController {
+    @GetMapping("admin/hello")
+    public String hello() {
+        return "adminHello";
+    }
+    @GetMapping("user/hello")
+    public String hello02() {
+        return "userHello";
+    }
+
+}
---
+++ src/main/java/com/example/security/MyWebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author zhoudb
+ * @date 2019/12/23 16:59
+ */
+@Configuration
+public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("123").roles("ADMIN", "USER")
+                .and()
+                .withUser("sang").password("123").roles("USER")
+               ;
+    }
+    @Override
+    protected void configure (HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
+                .antMatchers("/user/**")
+                .access("hasAnyRole('ADMIN','USER')")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable()
+        ;
+    }
+
+}
